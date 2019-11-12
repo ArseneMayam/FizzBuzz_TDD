b510f195b0c87226bd438f0a71841e9ffff64197
Mayam <arsene.mayam@cgi.com>
2019-11-12 16:16:01 -0500

    Initial commit

---
+++ FizzBuzz.Library.Test/FizzBuzzerTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz.Library.Tests
+{
+    [TestFixture]
+    public class FizzBuzzerTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Return_One()
+        {
+            // Arrange
+            int input = 1;
+            // Act
+            string value = FizzBuzzer.GetValue(input);
+            // Assert
+            Assert.AreEqual("1", value);
+        }
+
+        [Test]
+        public void DivBy3_ReturnsFizz([Values(3,6,9)]int input)
+        {            
+            string value = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("Fizz", value);
+        }
+
+        [Test]
+        public void DivBy5_ReturnsBuzz([Values(5,10,25)] int input)
+        {
+            string value = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("Buzz", value);
+        }
+
+        [Test]
+        public void DivBy3_DivBy5_ReturnsFizzBuzz([Values(15)]int input)
+        {
+            string value = FizzBuzzer.GetValue(input);
+            Assert.AreEqual("FizzBuzz", value);
+        }
+    }
+}
---
+++ FizzBuzz.Library/FizzBuzzer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz.Library
+{
+    public class FizzBuzzer
+    {
+        public static string GetValue(int input)
+        {
+            string value = string.Empty;
+            if (input % 3 == 0)
+                value += "Fizz";
+            if (input % 5 == 0)
+                value += "Buzz";
+            if (string.IsNullOrEmpty(value))
+                value = input.ToString();
+
+            return value;
+        }
+    }
+}
